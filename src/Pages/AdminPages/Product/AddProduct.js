13ead48b11065ae0ffc0d2223ece6c87d47bbdfb
Sanjay Shrestha <sanjayshrestha969@gmail.com>
2024-01-27 11:51:19 +0545

    add product sku

--- src/Pages/AdminPages/Product/AddProduct.js
+++ src/Pages/AdminPages/Product/AddProduct.js
@@ -244,7 +244,7 @@ function AddProduct({ modalIsOpen, closeModal, getRoute }) {
                                 <Field
                                   onChange={(e) => {
                                     props.handleChange(e)
-                                    props.setFieldValue(`variant.${index}.sku`, `${value.variant_type[0].size}-${value.variant_type[0].color}`)
+                                    props.setFieldValue(`variant.${index}.sku`, `${value.variant_type[0].size}-${e.target.value}`)
                                   }}
                                   className="block mt-2 w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                   name={`variant.${index}.variant_type[0].color`} />
